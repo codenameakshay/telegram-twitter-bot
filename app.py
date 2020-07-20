ce93a2090d886a429fa85cca20a8116e3204f4a4
CodeNameAkshay <akshaym.ec18@nsut.ac.in>
2020-07-21 01:49:53 +0530

    added more fns

--- app.py
+++ app.py
@@ -40,41 +40,91 @@ def respond():
        """
         bot.sendMessage(chat_id=chat_id, text=bot_welcome,
                         reply_to_message_id=msg_id)
-    elif text[0:6] == "/tweet":
-        text = text[6:len(text)]
-        def copyright_apply(input_image_path,
-                            output_image_path,
-                            text):
-            photo = Image.open(input_image_path)
-            w, h = photo.size
-            drawing = ImageDraw.Draw(photo)
-            font = ImageFont.truetype("Roboto.ttf", 72)
-            text_w, text_h = drawing.textsize(text, font)
-            pos = (w - text_w)-50, (h - text_h) - 50
-            c_text = Image.new('RGB', (text_w, (text_h)), color='#000000')
-            drawing = ImageDraw.Draw(c_text)
-            drawing.text((0, 0), text, fill="#ffffff", font=font)
-            c_text.putalpha(75)
-            photo.paste(c_text, pos, c_text)
-            photo.save(output_image_path)
-        url = urlRegex.search(text).group()
-        r = requests.head(url, allow_redirects=True)
-        imageUrl = r.url.split('socialImageUrl=')[1].split('&')[
-            0].replace('thumb_', '')
-        filename = 'temp.jpg'
-        req = requests.get(imageUrl, stream=True)
-        if req.status_code == 200:
-            with open(filename, 'wb') as image:
-                for chunk in req:
-                    image.write(chunk)
-            copyright_apply(filename, filename,
-                            text='@PrismWallpapers'
-                            )
-            status = api.update_with_media(filename, text)
-            bot.sendMessage(chat_id=chat_id, text="Tweeted successfully!", reply_to_message_id=msg_id)
-        else:
-            print("Unable to download image")
-            bot.sendMessage(chat_id=chat_id, text="Tweet Failed!", reply_to_message_id=msg_id)
+    elif text[0:6] == "/link ":
+        try:
+            def copyright_apply(input_image_path,
+                                output_image_path,
+                                text):
+                photo = Image.open(input_image_path)
+                w, h = photo.size
+                drawing = ImageDraw.Draw(photo)
+                font = ImageFont.truetype("Roboto.ttf", 72)
+                text_w, text_h = drawing.textsize(text, font)
+                pos = (w - text_w)-50, (h - text_h) - 50
+                c_text = Image.new('RGB', (text_w, (text_h)), color='#000000')
+                drawing = ImageDraw.Draw(c_text)
+                drawing.text((0, 0), text, fill="#ffffff", font=font)
+                c_text.putalpha(75)
+                photo.paste(c_text, pos, c_text)
+                photo.save(output_image_path)
+            url = urlRegex.search(text).group()
+            r = requests.head(url, allow_redirects=True)
+            imageUrl = r.url.split('socialImageUrl=')[1].split('&')[
+                0].replace('thumb_', '')
+            filename = 'temp.jpg'
+            req = requests.get(imageUrl, stream=True)
+            if req.status_code == 200:
+                with open(filename, 'wb') as image:
+                    for chunk in req:
+                        image.write(chunk)
+                copyright_apply(filename, filename,
+                                text='@PrismWallpapers'
+                                )
+                num = random.randint(0, 4)
+                randomText = ["Sometimes happy little accidents are the push you need for a spark of life. Open up the app for our latest Wallpapers.", "The beautiful nature of life is what keeps us thriving. Re-inspire your life with this and more awesome wallpapers.", "Kicking off the day in style is an art. You know what also is an art?\n\nThis wallpaper😊😊.",
+                              "Hard to resist, easy to apply. More amazing wallpapers on the app, download now. https://play.google.com/store/apps/details?id=com.hash.prism", "Live life to the fullest, and experience the best wallpapers with our exclusive collection featuring over 300+ walls filled with awesomeness. Download the app now for more such walls."]
+                status = api.update_with_media(filename, randomText[num]+" "+url)
+                bot.sendMessage(
+                    chat_id=chat_id, text="Tweeted successfully!", reply_to_message_id=msg_id)
+            else:
+                print("Unable to download image")
+                bot.sendMessage(
+                    chat_id=chat_id, text="Tweet Failed!", reply_to_message_id=msg_id)
+        except Exception as exc:
+            bot.sendMessage(
+                chat_id=chat_id, text="Some error in the script."+str(exc), reply_to_message_id=msg_id)
+    elif text[0:7] == "/tweet ":
+        try:
+            text = text[7:len(text)]
+
+            def copyright_apply(input_image_path,
+                                output_image_path,
+                                text):
+                photo = Image.open(input_image_path)
+                w, h = photo.size
+                drawing = ImageDraw.Draw(photo)
+                font = ImageFont.truetype("Roboto.ttf", 72)
+                text_w, text_h = drawing.textsize(text, font)
+                pos = (w - text_w)-50, (h - text_h) - 50
+                c_text = Image.new('RGB', (text_w, (text_h)), color='#000000')
+                drawing = ImageDraw.Draw(c_text)
+                drawing.text((0, 0), text, fill="#ffffff", font=font)
+                c_text.putalpha(75)
+                photo.paste(c_text, pos, c_text)
+                photo.save(output_image_path)
+            url = urlRegex.search(text).group()
+            r = requests.head(url, allow_redirects=True)
+            imageUrl = r.url.split('socialImageUrl=')[1].split('&')[
+                0].replace('thumb_', '')
+            filename = 'temp.jpg'
+            req = requests.get(imageUrl, stream=True)
+            if req.status_code == 200:
+                with open(filename, 'wb') as image:
+                    for chunk in req:
+                        image.write(chunk)
+                copyright_apply(filename, filename,
+                                text='@PrismWallpapers'
+                                )
+                status = api.update_with_media(filename, text)
+                bot.sendMessage(
+                    chat_id=chat_id, text="Tweeted successfully!", reply_to_message_id=msg_id)
+            else:
+                print("Unable to download image")
+                bot.sendMessage(
+                    chat_id=chat_id, text="Tweet Failed!", reply_to_message_id=msg_id)
+        except Exception as exc:
+            bot.sendMessage(
+                chat_id=chat_id, text="Some error in the script."+str(exc), reply_to_message_id=msg_id)
     else:
         bot.sendMessage(chat_id=chat_id, text=text, reply_to_message_id=msg_id)
     return 'ok'
